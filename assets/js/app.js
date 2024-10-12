fb33ca64463f22544f4416a0ea6a92298c6069bf
Andrea Damian <damian.moran.andrea@gmail.com>
2024-10-11 20:01:17 -0600

    cambios al código, tarea

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,16 +1,17 @@
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
+const $n = document.querySelector('#name'); /* CREO que name es selector, le puse uno de ID */
 const $b = document.querySelector('#blog');
-const $l = document.querySelector('.location');
+const $l = document.querySelector('#location');  /* también arreglé el # de location para que aparezca */
 
-function displayUser(username) {
-  $n.textContent = 'cargando...';
+async function displayUser(username) {    /* Hacía falta el Async para que funcione el await */
+  $n.textContent = 'cargando...'; 
   const response = await fetch(`${usersEndpoint}/${username}`);
+  const data = await response.json();  /* falta definir el bloque de abajo como json */
   console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
+  $n.textContent = `${data.name}`;
+  $b.textContent = `${data.blog}`;
+  $l.textContent = `${data.location}`;    /* Cambié comillas simples por back ticks  */
 }
 
 function handleError(err) {
